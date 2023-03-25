1dafe05a750e4a20b86e31f801f76581b8886662
corbatsh <bortlikkamil@gmail.com>
2023-03-25 11:28:39 +0100

    do while

---
+++ Zad3/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+#define N 10 //tymczasowe
+using namespace std;
+
+int main() {
+    int t[N]; //będą zmiany na float
+    int i, szukana; //inne zmienne w miarę potrzeby
+    bool znaleziono;
+
+    cout << "Program testuje algorytm ............" << endl;
+
+    //poniżej generowanie danych
+    srand(time(NULL));
+    for (i = 0; i < N; i++) {
+        cout << "t[" << i << "]=";
+        cin >> t[i];
+        //t[i] = rand() % 32700;
+
+        //  t[i]=i;
+        //  t[i]=-i;
+        //  t[i]=5;
+        //cout << "tpyrka[" << i << "]=" << t[i] << endl;
+    }
+
+    //poniżej testowany algorytm
+
+    cout << "podaj szukana: ";
+    cin >> szukana;
+    znaleziono = false;
+    i = 0;
+    do
+    {
+        if (t[i] == szukana)
+            znaleziono = true;
+        i++;
+    } while (!znaleziono && i < N);
+
+    if (!znaleziono)
+        cout << "nie wystapila szukana liczba";
+    else
+        cout << "wystapila szukana liczba";
+    //koniec algorytmu
+
+    //tu ewentualnie wyniki, komunikaty...
+
+    return 0;
+}
