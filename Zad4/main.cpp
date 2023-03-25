c120f2b96c2f114a4b432f95d50b2605bd14666f
corbatsh <bortlikkamil@gmail.com>
2023-03-25 17:22:41 +0100

    druga wersja funkcji pole_obw

--- Zad4/main.cpp
+++ Zad4/main.cpp
@@ -31,6 +31,16 @@ void pole_obw1(float a, float b, float &p, float &o)
     o=2*a+2*b;
 }
 
+//kolejna wersja
+bool pole_obw2(float a, float b, float &p, float &o)
+{
+    if (a <= 0 || b<=0)
+        return false;
+    p=a*b;
+    o=2*a+2*b;
+    return true;
+}
+
 int main() {
 
     float aa, bb, pole, obwod;
@@ -59,5 +69,16 @@ int main() {
     // funkcja ktora przyjmuje za parametry adres zmiennych (ampersand) odwoluja sie do adresu
     // zmiennej ktora zostanie tam przekazana i wlasnie na niej pracuja, a nie jak przy zwyklej
     // zmiennej, czyli nie kopiuja. Nazywa sie to przekazywanie przez referencje
+
+    // kolejna wersja
+    if( pole_obw2(aa, bb, pole, obwod) )
+    {
+        cout << "Pole = " << pole << endl;
+        cout << "Obw = " << obwod << endl;
+    }
+    else
+        cout << "Co najmniej jeden bok zly";
+    // te funkcje nie maja uprawnien do pracy z interfejsem (klawiatura, ekran itp..)
+    // gdyby tak było, nie mają przyszłości. Nie zastosuję ich w żadnych innych programach
     return 0;
 }
