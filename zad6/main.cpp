bee8d1dc4b437ead566e841e233ffcca66fa7454
corbatsh <bortlikkamil@gmail.com>
2023-04-16 08:34:36 +0200

    poprawki

--- zad6/main.cpp
+++ zad6/main.cpp
@@ -2,19 +2,17 @@
 #include <cstdlib>
 #include <ctime>
 
-#define N 10 //tymczasowe
 using namespace std;
 bool czyJest(int tablica[], int n, int szukana);
 int main() {
-    int t[N]; //będą zmiany na float
-    int i, szukana; //inne zmienne w miarę potrzeby
-    bool znaleziono;
-
+    int i, szukana, n, *t; //inne zmienne w miarę potrzeby
     cout << "Program testuje algorytm ............" << endl;
-
+    cout << "Ile elementow? ";
+    cin >> n;
+    t = new int[n];
     //poniżej generowanie danych
     srand(time(NULL));
-    for (i = 0; i < N; i++) {
+    for (i = 0; i < n; i++) {
         cout << "t[" << i << "]=";
         cin >> t[i];
         //t[i] = rand() % 32700;
@@ -25,15 +23,11 @@ int main() {
         //cout << "tpyrka[" << i << "]=" << t[i] << endl;
     }
 
-    //zakładając istnienie takiej n-elementowej tablicy zawierającej dane, wczytać wartość szukaną i przeszukując
-    // tablice wypisywać indeksy pod którymi ta szukana występuje. Gdy nie występuje w ogóle w tablicy, to wypisuje
-    // stosowny komunikat
-
     //poniżej testowany algorytm
 
     cout << "Podaj szukana wartosc: ";
     cin >> szukana;
-    if (czyJest(t, N, szukana))
+    if (czyJest(t, n, szukana))
         cout << "Znalazlem";
     else
         cout << "Nie znalazlem";
@@ -57,4 +51,5 @@ bool czyJest(int tablica[], int n, int szukana)
         }
         i++;
     } while (i <= n);
+    return znaleziono;
 }
