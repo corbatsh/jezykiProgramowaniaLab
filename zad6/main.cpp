354639dfa75e2703c601b3a77908f5737815503f
corbatsh <bortlikkamil@gmail.com>
2023-05-12 18:26:06 +0200

    szukaj function

--- zad6/main.cpp
+++ zad6/main.cpp
@@ -8,23 +8,49 @@ int minimum (int t[], int n);
 void minMax1 (int t[], int n, int &min, int &max);
 void minMax2 (int t[], int n, int *min, int *max);
 void selectionSort (int t[], int n);
+int szukaj (int t[], int n, int a, int b, int znalezione[]);
+
 int main() {
-    int i, szukana, n, *t, minim, maksim; //inne zmienne w miarę potrzeby
+    int i, szukana, n, *t, minim, maksim, *znalezione, iloscZnalezionych, a, b; //inne zmienne w miarę potrzeby
+    int resultSize = 0;
     cout << "Program testuje algorytm ............" << endl;
     cout << "Ile elementow? ";
     cin >> n;
     t = new int[n+1]; // dane w tablicy od indeksu 1 do n
+    znalezione = new int[n+1]; // Dynamically allocate memory for znalezione array
     //poniżej generowanie danych
     srand(time(NULL));
     for (i = 1; i <= n; i++) { // dane w tablicy od indeksu 1 do n
         //cout << "t[" << i << "]=";
         // cin >> t[i];
-        t[i] = rand() % 32700;
-        //  t[i]=i;
+       //t[i] = rand() % 32700;
+          t[i]=i;
         //  t[i]=-i;
         //  t[i]=5;
         cout << "t[" << i << "]=" << t[i] << endl;
     }
+    cout << "Podaj przedzial od do: " << endl;
+    cout << "a: ";
+    cin >> a;
+    cout << "b: ";
+    cin >> b;
+    iloscZnalezionych = szukaj(t, n, a, b, znalezione);
+    if (iloscZnalezionych)
+    {
+        cout << "znalezione: "<< iloscZnalezionych << endl;
+        for (int i = 1; i <= iloscZnalezionych; i++)
+        {
+            cout << "znaleziona[" << i << "]" << znalezione[i] << endl;
+        }
+    }
+    else
+        cout << "Brak znalezionych";
+
+
+
+
+    delete[] znalezione; // Free the dynamically allocated memory
+
     cout << "Wartosc minimalna w tablicy to: " << minimum(t,n) << endl;
 
     cout << "wywolano minMax1:" << endl;
@@ -124,6 +150,20 @@ void selectionSort (int t[], int n) //zachowuje sie bardzo stabilnie - posortowa
         t[indeks] = t[nr];
         t[nr] = min;
     }
+}
+
+int szukaj(int t[], int n, int a, int b, int znalezione[])
+{
+    int licznik = 0;
+    for (int i = 0; i <= n; i++)
+    {
+        if (t[i] >= a && t[i] <= b)
+        {
+            licznik++;
+            znalezione[licznik] = t[i]; // wazna jest tutaj inkrementacja licznika przed wsadzeniem do znalezionych
+        }
+    }
+    return licznik;
 }
 
 // opt = O(N^2)
